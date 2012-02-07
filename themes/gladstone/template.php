1042d4127e038a31e24aef3e469fc66de8e3e520
Erin Marchak <erin@emarchak.com>
2012-02-07 06:46:21 -0500

    only show one image on teaser

--- themes/gladstone/template.php
+++ themes/gladstone/template.php
@@ -74,3 +74,20 @@ function gladstone_panels_default_style_render_region($vars) {
 //	$output .= '</div>';
 	return $output;
 }
+
+
+function gladstone_process_field(&$vars) {
+  $element = $vars['element'];
+
+  // Field type image
+  if ($element['#field_type'] == 'media') {
+
+    // Reduce number of images in teaser view mode to single image
+    if ($element['#view_mode'] == 'teaser') {
+      $item = reset($vars['items']);
+      $vars['items'] = array($item);
+    }
+
+  }
+
+}
